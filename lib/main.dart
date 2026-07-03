f7b16521385368f9c5b66c22a346202269e70d34
Roman Dzerkal <dzerkal.r@gmail.com>
2026-07-03 18:09:17 +0300

    [new] init project

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xFFF7F1EB),
+        primaryColor: Color(0xFF8B6B4F)
+      ),
+    ),
+  );
+}
